2da3d22970e6d887de9c059d78e66c397f1b2945
nguyenxuantuan <=>
2018-12-24 11:45:09 +0700

    add bootstrap

--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-Session.create(exam_name: "kyI", period_for: "60", supervisor: "tuan", create_at: "", updated_at: "")
-Session.create(exam_name: "kyII", period_for: "20", supervisor: "tuan", create_at: "", updated_at: "")
-Session.create(exam_name: "kyIII", period_for: "30", supervisor: "thanh", create_at: "", updated_at: "")
-Session.create(exam_name: "kyIV", period_for: "60", supervisor: "thanh", create_at: "", updated_at: "")
-Session.create(exam_name: "kyV", period_for: "60", supervisor: "tuan", create_at: "", updated_at: "")
